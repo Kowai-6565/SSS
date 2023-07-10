c0506a1c5464c82fea8a4c94bfe8b2120732fa01
SaktanuthPeak <thanakrit.5122548@gmail.com>
2023-07-10 16:39:09 +0700

    Update Southern.py

--- Southern.py
+++ Southern.py
@@ -6,7 +6,7 @@ import os
 import os.path as op
 from torchvision import models, transforms
 st.set_page_config(layout="centered")
-st.title("Thai food image classification")
+st.title("Pak-tAI")
 
 st.write("")
 file_up = st.file_uploader("Upload an image", type="jpg")
@@ -20,64 +20,29 @@ transform_test = transforms.Compose([
 model = models.resnet34()
 num_features = model.fc.in_features
 model.fc = torch.nn.Linear(num_features, 54)  
-model.load_state_dict(torch.load("Finalmodel.pth", map_location=torch.device('cpu')))
+model.load_state_dict(torch.load("Southern.pth", map_location=torch.device('cpu')))
 model.eval()
 
 class_names = {
- 0:"ก๋วยเตี๋ยวเรือ",
- 1:"หอยเเครงลวก",
- 2:"กุ้ยช่าย",
- 3:"ยําปลาดุกฟู",
- 4:"กะหรี่พัฟ",
- 5:"หอยจ๊อ",
- 6:"กุ้งเเม่นํ้า",
- 7:"ขนมไข่เต่า",
- 8:"ขนมครก",
- 9:"กุ้งเเช่นํ้าปลา",
- 10:"มะม่วงนํ้าปลาหวาน",
- 11:"ข้าวไข่เจียว",
- 12:"ปลากระพงนึ่งมะนาว",
- 13:"ราดหน้า",
- 14:"ห่อหมกปลา",
- 15:"ข้าวขาหมู",
- 16:"ผัดหอยเเครง",
- 17:"ไก่ผัดเม็ดมะม่วงหิมพานต์",
- 18:"เเกงไตปลา",
- 19:"ยําวุ้นเส้น",
- 20:"เย็นตาโฟ",
- 21:"ข้าวผัดอเมริกัน",
- 22:"เเกงจืดมะระ",
- 23:"บัวลอย",
- 24:"ปูผัดผงกะหรี่",
- 25:"ไก่ทอด",
- 26:"ปอเปี้ยทอด",
- 27:"ปลาหมึกย่าง",
- 28:"ขนมชั้น",
- 29:"ขนมตาล",
- 30:"ไข่พะโล้",
- 31:"ข้าวคลุกกะปิ",
- 32:"ข้าวหมกไก่",
- 33:"ข้าวหน้าเป็ด",
- 34:"ข้าวซอย",
- 35:"ข้าวยํา",
- 36:"ลาบ",
- 37:"ข้าวเหนียวมะม่วง",
- 38:"เเกงมัสมั่น",
- 39:"หมูกรอบ",
- 40:"ผัดซีอิ้ว",
- 41:"ผัดไทย",
- 42:"ข้าวผัดสับปะรด",
- 43:"ปลาสามรส",
- 44:"กระเพราหมู",
- 45:"ไส้อั่ว",
- 46:"ปลาย่างเกลือ",
- 47:"ส้มตํา",
- 48:"ไข่ลูกเขย",
- 49:"ผัดผักบุ้ง",
- 50:"ผัดสะตอ",
- 51:"ทอดมันปลา",
- 52:"เเกงข่าไก่",
- 53:"ต้มยํากุ้ง"
+ 0:"0",
+ 1:"1",
+ 2:"2",
+ 3:"3",
+ 4:"4",
+ 5:"5",
+ 6:"6",
+ 7:"7",
+ 8:"8",
+ 9:"9",
+ 10:"10",
+ 11:"11",
+ 12:"12",
+ 13:"13",
+ 14:"14",
+ 15:"15",
+ 16:"16",
+ 17:"17",
+ 
 }
 
 model.food_class = class_names 
@@ -106,6 +71,12 @@ if file_up is not None:
         st.write('ชนิดอาหาร:', model.food_class[pred_id])
         if pred_id == 18:
             st.image('nutrition.jpg')
+        if pred_id == 18:
+            st.image('nutrition.jpg')
+        if pred_id == 18:
+            st.image('nutrition.jpg')
+        if pred_id == 18:
+            st.image('nutrition.jpg')            
 
     os.remove(temp_file_path)
 else:
