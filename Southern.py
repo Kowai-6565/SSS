063186e3d42b360be4854b61e32428c5effadea4
SaktanuthPeak <thanakrit.5122548@gmail.com>
2023-07-10 16:57:12 +0700

    Update Southern.py

--- Southern.py
+++ Southern.py
@@ -24,30 +24,28 @@ model.load_state_dict(torch.load("Southern.pth", map_location=torch.device('cpu'
 model.eval()
 
 class_names = {
- 0:"0",
- 1:"1",
- 2:"2",
- 3:"3",
- 4:"4",
- 5:"5",
- 6:"6",
- 7:"7",
- 8:"8",
- 9:"9",
- 10:"10",
- 11:"11",
- 12:"12",
- 13:"13",
- 14:"14",
- 15:"15",
- 16:"16",
- 17:"17",
+ 0:"ไก่ต้มขมิ้น",
+ 1:"เเกงคั่วหอยขมใบชะพลู",
+ 2:"ขนมจีนนํ้ายาปู",
+ 3:"ไก่กอเเละ",
+ 4:"ไก่ทอดหาดใหญ่",
+ 5:"ไข่ครอบ",
+ 6:"ข้าวยํา",
+ 7:"คั่วกลิ้งหมู",
+ 8:"ใบเหลียงต้มกะทิกุ้งสด",
+ 9:"หมูฮ้อง",
+ 10:"นํ้าพริกกะปิ",
+ 11:"นํ้าพริกกุ้งเสียบ",
+ 12:"ปลากรายทอดขมิ้น",
+ 13:"เเกงเหลืองปลากระพง",
+ 14:"ใบเหลียงผัดไข่",
+ 15:"หมูผัดกะปิ",
+ 16:"สะตอผัดกุ้ง",
+ 17:"เเกงไตปลา",
  
 }
 
 model.food_class = class_names 
-
-
 
 if file_up is not None:
 
@@ -69,14 +67,59 @@ if file_up is not None:
         _, predict = torch.max(outputs, 1)
         pred_id = predict.item()
         st.write('ชนิดอาหาร:', model.food_class[pred_id])
-        if pred_id == 18:
+        if pred_id == 0:
             st.image('nutrition.jpg')
-        if pred_id == 18:
+
+        if pred_id == 1:
             st.image('nutrition.jpg')
-        if pred_id == 18:
+
+        if pred_id == 2:
             st.image('nutrition.jpg')
-        if pred_id == 18:
-            st.image('nutrition.jpg')            
+
+        if pred_id == 3:
+            st.image('nutrition.jpg')
+
+        if pred_id == 4:
+            st.image('nutrition.jpg')
+
+        if pred_id == 5:
+            st.image('nutrition.jpg')
+
+        if pred_id == 6:
+            st.image('nutrition.jpg')
+
+        if pred_id == 7:
+            st.image('nutrition.jpg')
+
+        if pred_id == 8:
+            st.image('nutrition.jpg')
+
+        if pred_id == 9:
+            st.image('nutrition.jpg')
+
+        if pred_id == 10:
+            st.image('nutrition.jpg')
+
+        if pred_id == 11:
+            st.image('nutrition.jpg')
+
+        if pred_id == 12:
+            st.image('nutrition.jpg')
+
+        if pred_id == 13:
+            st.image('nutrition.jpg')
+
+        if pred_id == 14:
+            st.image('nutrition.jpg')
+
+        if pred_id == 15:
+            st.image('nutrition.jpg')
+
+        if pred_id == 16:
+            st.image('nutrition.jpg')
+
+        if pred_id == 17:
+            st.image('nutrition.jpg')  
 
     os.remove(temp_file_path)
 else:
